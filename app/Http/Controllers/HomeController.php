366e6e9153e0903e879aee89ed2db6f89503748e
Citra Aldona <citraaldona11@gmail.com>
2023-10-14 19:54:39 +0700

    Add Method datadiri

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index() {
+        return view('welcome');
+    }
+public function datadiri(){
+    return "Nama : Citra Aldona <br> NIM : D212011041 <br> Kelas : KA-6B"
 }
+}
+
